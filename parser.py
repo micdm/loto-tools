55bafedcfce96ed09c69fce09c3fb8ea7621a966
Mikhail Demerzov <demerzov@gmail.com>
2017-02-28 15:38:47 +0700

    new: парсер тиражей Русского лото

---
+++ parser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import sys
+from lxml import etree
+
+tree = etree.parse(sys.argv[1], etree.HTMLParser())
+for row in tree.xpath('//div[@class="data_table"]//tbody/tr'):
+    cells = row.xpath('./td')
+    round, numbers = int(cells[0].text), map(lambda item: int(item.strip()), cells[1].text.split(','))
+    for number in numbers:
+        print("{} {}".format(number, round))
+ 
