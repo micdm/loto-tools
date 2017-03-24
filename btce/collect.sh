4d86bcce2b93d3e14f37126ac9f54524a9782b47
mic <demerzov@gmail.com>
2017-03-24 09:58:06 +0000

    new: слегка оптимизируем сбор

--- btce/collect.sh
+++ btce/collect.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "Parsing..."
-for i in `find data -name "*.html"`; do ./parser.py $i; done > data/parsed.data
+for i in `find -L data -name "*.html"`; do 
+    cache=$i.data
+    if [ -f $cache ]; then
+        cat $cache
+    else
+        ./parser.py $i | tee $cache
+    fi
+done > data/parsed.data
 echo "Merging..."
 ./merger.py data/parsed.data > data/merged.data
--- btce/merger.py
+++ btce/merger.py
@@ -14,5 +14,5 @@ def get_rounds_from_file(file_name):
 
 rounds = get_rounds_from_file(sys.argv[1])
 
-for key in sorted(rounds.keys()):
+for key in sorted(rounds.keys(), key=int):
     print(json.dumps(rounds[key]))
