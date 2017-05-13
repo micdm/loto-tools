c362e55ab921148b98d83f7751969148955cff39
mic <demerzov@gmail.com>
2017-05-13 19:20:19 +0700

    new: пишем timestamp для каждого розыгрыша

--- btce/parser.py
+++ btce/parser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from datetime import datetime, timedelta
 import json
 import re
 import sys
@@ -17,6 +18,8 @@ def get_amounts(text):
 
 tree = etree.parse(sys.argv[1], etree.HTMLParser())
 table = str.maketrans({'↓': '', '↑': ''})
+
+today = datetime.strptime(tree.xpath('(//div[@class="orderStats"]/strong)[last()]')[0].text, '%d.%m.%y %H:%M').date()
 for row in tree.xpath('//table[@id="last_periods"]//tr'):
     cells = row.xpath('./td')
     if not cells:
@@ -24,10 +27,14 @@ for row in tree.xpath('//table[@id="last_periods"]//tr'):
     is_ended = len(cells[6].xpath('.//span[text()="ended"]')) != 0
     if not is_ended:
         continue
+    start_time = datetime.strptime(cells[1].text[:5], "%H:%M").time()
     print(json.dumps({
         'number': cells[0].text,
+        'start_time': str(datetime.combine(today, start_time).strftime('%s')),
         'start_price': cells[2].text,
         'end_price': cells[3].xpath('./b')[0].text.translate(table),
         'bets': get_bets(cells[4].text),
         'amounts': get_amounts(cells[5].text)
     }))
+    if start_time.hour == 0 and start_time.minute == 0:
+        today -= timedelta(days=1)
