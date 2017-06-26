36db350dbde8dcafa14fe2a0a50a785b2d5fd292
mic <demerzov@gmail.com>
2017-06-26 15:31:07 +0700

    rework: попроще

--- btce/daily.py
+++ btce/daily.py
@@ -3,53 +3,63 @@
 import json
 import sys
 
-DELTA_THRESHOLD = 1.05
+DELTA_THRESHOLD = 1.03
 PRIZE_PART = 0.998
 
 INPUT = sys.argv[1]
 CURRENCIES = ("btc", "ltc", "eth", "dsh")
 
 
-def get_pairs_as_first(first):
-    for currency in CURRENCIES:
-        key = "%s_%s" % (first, currency)
-        if key in current:
-            yield key
+def get_datas():
+    previous_timestamp = None
+    for line in open(INPUT):
+        data = json.loads(line)
+        timestamp = next(iter(data.values()))['updated']
+        if previous_timestamp and timestamp - previous_timestamp < 43200:
+            continue
+        previous_timestamp = timestamp
+        yield dict((key, value['last']) for key, value in data.items())
 
 
-def get_pairs_as_second(second):
-    for currency in CURRENCIES:
-        key = "%s_%s" % (currency, second)
-        if key in current:
-            yield key
+def get_all_deltas(base, previous, current):
+    for key in current.keys():
+        if key.startswith(base):
+            yield key, current[key] / previous[key]
+        if key.endswith(base):
+            yield key, previous[key] / current[key]
 
 
-balance = {"btc": 0.1}
-datas = tuple(map(lambda item: dict((key, value["last"]) for key, value in item.items()), (json.loads(line) for line in open(INPUT))))
-for previous, current in zip(datas, datas[1:]):
-    base = tuple(balance.keys())[0]
-    pairs_as_first = tuple(get_pairs_as_first(base))
-    pairs_as_second = tuple(get_pairs_as_second(base))
-    if pairs_as_first:
-        pair = max(pairs_as_first, key=lambda key: current[key] / previous[key])
-        if current[pair] < previous[pair]:
-            print("Price has been decreased, skipping")
-        elif current[pair] / previous[pair] < DELTA_THRESHOLD:
-            print("Price has been increased too low (%s), skipping" % (current[pair] / previous[pair]))
-        else:
+def get_preferred_deltas(base, deltas):
+    return filter(lambda item: any(currency != base and currency in item[0] for currency in CURRENCIES), deltas)
+
+
+def run():
+    datas = tuple(get_datas())
+    balance = {"btc": 0.1}
+    previous = datas[0]
+    for current in datas[1:]:
+        base = tuple(balance.keys())[0]
+        deltas = get_preferred_deltas(base, get_all_deltas(base, previous, current))
+        pair, value = max(deltas, key=lambda item: item[1])
+        print("The best pair is %s with delta %s" % (pair, value))
+        if value < DELTA_THRESHOLD:
+            print("Price has been changed too low, skipping")
+            continue
+        if pair.startswith(base):
             currency = pair.split("_")[1]
-            amount = (balance[base] * current[pair]) * PRIZE_PART
-            print("Selling %s for %s, amount is %s, delta is %s" % (base, currency, amount, current[pair] / previous[pair]))
+            amount = balance[base] * current[pair] * PRIZE_PART
+            print("Selling %s for %s, amount is %s" % (base, currency, amount))
             balance = {currency: amount}
-    elif pairs_as_second:
-        pair = max(pairs_as_second, key=lambda key: previous[key] / current[key])
-        if current[pair] > previous[pair]:
-            print("Price has been increased, skipping")
-        elif previous[pair] / current[pair] < DELTA_THRESHOLD:
-            print("Price has been decreased too low (%s), skipping" % (previous[pair] / current[pair]))
-        else:
+        elif pair.endswith(base):
             currency = pair.split("_")[0]
-            amount = (balance[base] / current[pair]) * PRIZE_PART
-            print("Buying %s for %s, amount is %s, delta is %s" % (currency, base, amount, previous[pair] / current[pair]))
+            amount = balance[base] / current[pair] * PRIZE_PART
+            print("Buying %s for %s, amount is %s" % (currency, base, amount))
             balance = {currency: amount}
-print("Final balance is %s" % balance)
+        else:
+            raise Exception("not supposed to happen")
+        previous = current
+    print("Final balance is %s" % balance)
+
+
+if __name__ == '__main__':
+    run()
